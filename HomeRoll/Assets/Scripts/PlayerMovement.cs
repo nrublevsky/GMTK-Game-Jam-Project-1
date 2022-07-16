aa87726819caabe2073f0f72351eb4c6b3e88ded
nrublevsky <nickrublevsky@gmail.com>
2022-07-16 17:30:14 +0200

    Update PlayerMovement.cs
    yep

--- HomeRoll/Assets/Scripts/PlayerMovement.cs
+++ HomeRoll/Assets/Scripts/PlayerMovement.cs
@@ -10,6 +10,8 @@ public class PlayerMovement : MonoBehaviour
     public float rotationSpeed;
     public float moveSpeed;
 
+    public bool isMoving;
+
     public bool gameStarted;
     void Start()
     {
@@ -17,6 +19,7 @@ public class PlayerMovement : MonoBehaviour
         rotationAngle = 100f;
         rotationSpeed = 3f;
         moveSpeed = 8f;
+        isMoving = false;
     }
 
     // Update is called once per frame
@@ -31,26 +34,52 @@ public class PlayerMovement : MonoBehaviour
         gameStarted = true;
         if (gameStarted)
         {
-            if (Input.GetKey(KeyCode.D))
+            if (!isMoving)
             {
-                playerRb.transform.Rotate(Vector3.right * rotationAngle * rotationSpeed * Time.deltaTime, Space.World);
-                playerRb.transform.Translate(Vector3.back * -moveSpeed * Time.deltaTime, Space.World);
-            }
-            if (Input.GetKey(KeyCode.A))
-            {
-                playerRb.transform.Rotate(Vector3.right * -rotationAngle * rotationSpeed * Time.deltaTime, Space.World);
-                playerRb.transform.Translate(Vector3.back * moveSpeed * Time.deltaTime, Space.World);
-            }
-            if (Input.GetKey(KeyCode.W))
-            {
-                playerRb.transform.Rotate(Vector3.forward * rotationAngle * rotationSpeed * Time.deltaTime, Space.World);
-                playerRb.transform.Translate(Vector3.right * -moveSpeed * Time.deltaTime, Space.World);
-            }
-            if (Input.GetKey(KeyCode.S))
-            {
-                playerRb.transform.Rotate(Vector3.forward * -rotationAngle * rotationSpeed * Time.deltaTime, Space.World);
-                playerRb.transform.Translate(Vector3.right * moveSpeed * Time.deltaTime, Space.World);
+                if (Input.GetKey(KeyCode.D))
+                {
+                    StartCoroutine(Move(Vector3.right,-Vector3.back));
+                    //playerRb.transform.Rotate(Vector3.right * rotationAngle * rotationSpeed * Time.deltaTime, Space.World);
+                    //playerRb.transform.Translate(Vector3.back * -moveSpeed * Time.deltaTime, Space.World);
+
+                }
+                if (Input.GetKey(KeyCode.A))
+                {
+                    StartCoroutine(Move(-Vector3.right, Vector3.back));
+                    //playerRb.transform.Rotate(Vector3.right * -rotationAngle * rotationSpeed * Time.deltaTime, Space.World);
+                    //playerRb.transform.Translate(Vector3.back * moveSpeed * Time.deltaTime, Space.World);
+                }
+                if (Input.GetKey(KeyCode.W))
+                {
+                    StartCoroutine(Move(Vector3.forward, -Vector3.right));
+                    //playerRb.transform.Rotate(Vector3.forward * rotationAngle * rotationSpeed * Time.deltaTime, Space.World);
+                    //playerRb.transform.Translate(Vector3.right * -moveSpeed * Time.deltaTime, Space.World);
+                }
+                if (Input.GetKey(KeyCode.S))
+                {
+                    StartCoroutine(Move(-Vector3.forward, Vector3.right));
+                    //playerRb.transform.Rotate(Vector3.forward * -rotationAngle * rotationSpeed * Time.deltaTime, Space.World);
+                    //playerRb.transform.Translate(Vector3.right * moveSpeed * Time.deltaTime, Space.World);
+                }
             }
         }
+    }
+
+    public void MoveInDirection(Vector3 rotationDirection, Vector3 movementDirection)
+    {
+        playerRb.transform.Rotate(rotationDirection * rotationAngle * rotationSpeed * Time.deltaTime, Space.World);
+        playerRb.transform.Translate(movementDirection * moveSpeed * Time.deltaTime, Space.World);
+    }
+
+    public IEnumerator Move(Vector3 rotationDirection, Vector3 movementDirection)
+    {
+        isMoving = true;
+        yield return new WaitForSeconds(1);
+        while (isMoving)
+        {
+            MoveInDirection(rotationDirection,movementDirection);
+        }
+        isMoving = false;
+
     }
 }
