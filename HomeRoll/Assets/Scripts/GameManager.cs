a9bfb2a7749d2b53013899a8aa11854c3ee0827d
nrublevsky <nickrublevsky@gmail.com>
2022-07-17 17:41:28 +0200

    updated to webgl

--- HomeRoll/Assets/Scripts/GameManager.cs
+++ HomeRoll/Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
  using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
@@ -15,6 +16,8 @@ public class GameManager : MonoBehaviour
 
     public bool gameRunning;
 
+    public string currentSceneName;
+
     //level data and objects
     public GameObject player;
 
@@ -27,6 +30,23 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        if (SceneManager.GetActiveScene().name == "Menu")
+        {
+
+        }
+        if (SceneManager.GetActiveScene().name == "Level1")
+
+        {
+
+        }
+        if (SceneManager.GetActiveScene().name == "Level2")
+        {
+
+        }
+        if (SceneManager.GetActiveScene().name == "Level3")
+        {
+
+        }
         FindAll();
         
     }
@@ -94,6 +114,8 @@ public class GameManager : MonoBehaviour
                 
         finishText.gameObject.SetActive(false);
         nextLevel.gameObject.SetActive(false);
+        restartGame.gameObject.SetActive(false);
+        quitGame.gameObject.SetActive(false);
     }
 
     public GameObject Player()
--- HomeRoll/Assets/Scripts/TileBehavior.cs
+++ HomeRoll/Assets/Scripts/TileBehavior.cs
@@ -55,23 +55,23 @@ public class TileBehavior : MonoBehaviour
 
         switch (tileState)
         {
-            case 0:
-                
-                break;
             case 1:
-
+                tileRenderer.material.color = new Color(174,255,165);
                 break;
             case 2:
-
+                tileRenderer.material.color = new Color(248, 255, 95);
                 break;
             case 3:
-
+                tileRenderer.material.color = new Color(255, 149, 124);
                 break;
             case 4:
-
+                tileRenderer.material.color = new Color(255, 133, 229);
                 break;
             case 5:
-
+                tileRenderer.material.color = new Color(153, 136, 255);
+                break;
+            case 6:
+                tileRenderer.material.color = new Color(85, 255, 237);
                 break;
             default:
 
