5cd3b5d4a84972136d53c74a06eccfb6203b592f
nrublevsky <nickrublevsky@gmail.com>
2022-07-17 16:32:51 +0200

    Added UI controls in Main Manager
    Finish text
    Time field
    Next level Button

--- HomeRoll/Assets/Scripts/GameManager.cs
+++ HomeRoll/Assets/Scripts/GameManager.cs
@@ -6,10 +6,12 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     //universal data and objects
-    public int score;
-
-    public Text playerScore;
+    public Time timeCountdown;
     public Text timeCounter;
+    public Text finishText;
+    public Button nextLevel;
+
+    public bool gameRunning;
 
     //level data and objects
     public GameObject player;
@@ -35,21 +37,64 @@ public class GameManager : MonoBehaviour
     //methods for update
     public void ControlSituation() 
     {
-        CheckFloor();
-       
+        
+        if (gameRunning)
+        {
+            CheckFloor(GetFloor());
+        }
+        if (!gameRunning)
+        {
+            UpdateUI();
+        }
+        
+        
     }
-    public void CheckFloor()
+
+    public GameObject GetFloor()
+    {
+        return floor;
+    }
+
+    public void CheckFloor(GameObject floor)
+    {
+        FloorManager flManager = floor.GetComponent<FloorManager>();
+        tilesLeft = flManager.tilesList.Count;
+        if (tilesLeft < 1)
+        {
+            gameRunning = false;
+        }
+    }
+
+    public void UpdateUI()
     {
         
+        if (!gameRunning)
+        {
+            
+
+            finishText.gameObject.SetActive(true);
+            nextLevel.gameObject.SetActive(true);
+        }
     }
     
     //methods for start
 
     public void FindAll()
     {
+        
+
+        SetUpGame();
         Player();
         Floor();
     } 
+
+    public void SetUpGame()
+    {
+        gameRunning = true;
+                
+        finishText.gameObject.SetActive(false);
+        nextLevel.gameObject.SetActive(false);
+    }
 
     public GameObject Player()
     {
--- HomeRoll/Assets/Scripts/TileBehavior.cs
+++ HomeRoll/Assets/Scripts/TileBehavior.cs
@@ -16,8 +16,7 @@ public class TileBehavior : MonoBehaviour
     {
         tilePresent = true;
         dice = GameObject.Find("Player");
-        
-        
+               
         sideBehavior = dice.GetComponentInChildren<DiceSideBehavior>();
 
     }
@@ -50,82 +49,10 @@ public class TileBehavior : MonoBehaviour
     public void CheckState()
     {
         switch (tileState)
-        {
-            case 0:
-                
-            case 1:
-                SetFireTile();
-                break;
-            case 2:
-
-            case 3:
-                SetWaterTile();
-                break;
-            case 4:
-
-            case 5:
-                SetAirTile();
-                break;
-            case 6:
-
-            case 7:
-                SetGroundTile();
-                break;
-            case 8:
-
-            case 9:
-                SetWoodTile();
-                break;
-            case 10:
-
-            case 11:
-                SetIceTile();
-                break;
-            case 12:
-                SetLavaTile();
-                break;
-            case 13:
-                
-            case 14:
-                SetVoidTile();
-                break;
-            case 15:    
-
-                break;
+        {          
                 
         }
         
     }
-    public void SetFireTile()
-    {
 
-    }
-    public void SetWaterTile()
-    {
-
-    }
-    public void SetAirTile()
-    {
-
-    }
-    public void SetGroundTile()
-    {
-
-    } 
-    public void SetWoodTile()
-    {
-
-    } 
-    public void SetIceTile()
-    {
-
-    }
-    public void SetLavaTile()
-    {
-
-    }
-    public void SetVoidTile()
-    {
-
-    }
 }
